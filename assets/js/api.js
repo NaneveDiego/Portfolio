50415984129fc849fb48a1f1666b7c1dc9d4ee05
unknown <nanevediego>
2024-04-23 22:21:10 -0300

    tec3

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData(){
-    const url = 'https://raw.githubusercontent.com/NaneveDiego/js-developer-portfolio/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/NaneveDiego/Portfolio/main/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json();
 
