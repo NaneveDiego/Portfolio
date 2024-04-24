e08cb0224f043b2db989d159248153378cd28303
unknown <nanevediego>
2024-04-23 22:42:45 -0300

    tec6

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,8 +2,8 @@
 
 async function fetchProfileData(){
     const url = 'https://raw.githubusercontent.com/NaneveDiego/Portfolio/main/data/profile.json'
-    const fetching = await fetch(url)
-    return await fetching.json();
-
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
 
 }
